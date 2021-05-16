065b6bc9b2b27f792dac071fcd74b49f15ca9a2d
timhatton <tim@git.hub>
2021-05-16 18:29:14 +0100

    Try with identity

--- src/App.svelte
+++ src/App.svelte
@@ -7,6 +7,31 @@
 	import About from './About.svelte';
     import "../node_modules/modern-css-reset/dist/reset.min.css";
 	import "./styles.css";
+	import netlifyIdentity from 'netlify-identity-widget'
+	import { user, redirectURL } from './identityStore.js'
+
+	netlifyIdentity.init();
+
+	$: isLoggedIn = !!$user
+  	$: username = $user !== null ? $user.username : ' there!'
+
+	  function handleUserAction(action) {
+    if (action === 'login' || action === 'signup') {
+      netlifyIdentity.open(action)
+      netlifyIdentity.on('login', u => {
+        user.login(u)
+        netlifyIdentity.close()
+        if ($redirectURL !== '') {
+          navigate($redirectURL)
+          redirectURL.clearRedirectURL()
+        }
+      })
+    } else if (action === 'logout') {
+      navigate('/')
+      user.logout()
+      netlifyIdentity.logout()
+    }
+  }
 
 	let page
 	let params;
@@ -29,6 +54,22 @@
 </script>
 
 <main>
+	{#if isLoggedIn}
+    <div class="center">
+      <p>Hello {username}</p>
+      <div>
+        <button on:click={() => handleUserAction('logout')}>Log Out</button>
+      </div>
+    </div>
+  {:else}
+    <div class="center">
+      <p>You are not logged in.</p>
+      <div>
+        <button on:click={() => handleUserAction('login')}>Log In</button>
+        <button on:click={() => handleUserAction('signup')}>Sign Up</button>
+      </div>
+    </div>
+  {/if}
 	<svelte:component this={page}  params={params}/>
 </main>
 
